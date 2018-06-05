71f04eadaf78e564c4827c281baa240c3c30f7e7
Hans <j.h.vanderlaan@student.utwente.nl>
2018-06-05 15:06:03 +0200

    Code is broken, however this is for illustration

--- src/battleship/CommandCenter.py
+++ src/battleship/CommandCenter.py
@@ -2,44 +2,43 @@ class CommandCenter():
     hatch_open = False
     shells_count = 0
     target_locked = False
-    cannon_count = 8
-    cannon_angles = [0,0,0,0,0,0,0,0]
-    cannon_tilts = [0,0,0,0,0,0,0,0]
+    cannon_angle = 0
+    cannon_tilt = 0
 
     def __init__(self, shell_count):
         self.shells_count = shell_count
 
-    def fire_canon(self, cannon_nb, angle, distance):
+    def fire_canon(self, angle, distance):
         if (not self.hatch_open and self.shells_count >= 1):
             if (self.target_locked):
-                self.turn_cannon(cannon_nb, angle)
-                tilt_angle = self.calculate_angle(cannon_nb, distance)
-                self.tilt_cannon(cannon_nb, tilt_angle)
-                self.fire_shell(cannon_nb)
+                self.turn_cannon( angle)
+                tilt_angle = self.calculate_angle(distance)
+                self.tilt_cannon( tilt_angle)
+                self.fire_shell()
                 return 0
             else:
                 return 1
         else:
             return 2
 
-    def turn_cannon(self, cannon_nb, angle):
-        print("turning cannon " + str(cannon_nb) + " from " +
-              str(self.cannon_angles[cannon_nb]) + "° to " + str(angle) + "°")
-        self.cannon_angles[cannon_nb] = angle
+    def turn_cannon(self, angle):
+        print("turning cannon from " +
+              str(self.cannon_angle) + "° to " + str(angle) + "°")
+        self.cannon_angle = angle
 
 
-    def tilt_cannon(self, cannon_nb, angle):
-        print("tilting cannon " + str(cannon_nb) + " from " +
-              str(self.cannon_tilts[cannon_nb]) + "° to " + str(angle) + "°")
-        self.cannon_tilts[cannon_nb] = angle
+    def tilt_cannon(self, tilt):
+        print("tilting cannon from " +
+              str(self.cannon_tilt) + "° to " + str(tilt) + "°")
+        self.cannon_angle = tilt
 
-    def calculate_angle(self, cannon_nb, distance):
+    def calculate_angle(self, distance):
         return distance / 10
 
 
-    def fire_shell(self, cannon_nb):
+    def fire_shell(self):
         self.shells_count += -1
-        print("cannon " + str(cannon_nb) + " does 'Phew'!")
+        print("the cannon does 'Phew'!")
         print("there are now " + str(self.shells_count) + " shells left")
 
     def lock_target(self):
